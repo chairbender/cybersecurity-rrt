07727b4b116ef11d6a43f0ac801a5cac6d03156a
chairbender <kwhipke1@gmail.com>
2022-04-12 22:15:20 -0700

    test multiple cases using parameterized tests

--- cybersecurity-rrt-logic/src/game/logic.rs
+++ cybersecurity-rrt-logic/src/game/logic.rs
@@ -3,7 +3,7 @@ use crate::defs;
 use crate::defs::OperatorType;
 use crate::game::ChoiceState::ChooseAction;
 use crate::game::Difficulty::Easy;
-use crate::game::{HackerCard, HackerDeck, OperatorState};
+use crate::game::{Difficulty, HackerCard, HackerDeck, OperatorState};
 use arrayvec::ArrayVec;
 use rand::seq::{IteratorRandom, SliceRandom};
 use rand::Rng;
@@ -13,15 +13,9 @@ use rand::Rng;
 /// Returns a tablestate fully setup in accordance with
 /// the provided game config, ready for the first operator to perform their turn.
 pub fn setup_game(config: &GameConfig) -> TableState {
-    let (barricade_mod, hacker_mult) = match config.difficulty() {
-        Easy => (3, 6),
-        Normal => (2, 7),
-        Hard => (1, 7),
-        Heroic => (0, 7),
-    };
-
+    let (firewall_mod, hacker_mult) = difficulty_mod(config.difficulty());
     TableState {
-        firewalls: (config.operators().len() + barricade_mod) as u8,
+        firewalls: (config.operators().len() + firewall_mod) as u8,
         databases: [true; 3],
         webservices: [true; 6],
         hackers: shuffle(config.operators().len() * hacker_mult),
@@ -31,6 +25,15 @@ pub fn setup_game(config: &GameConfig) -> TableState {
         active_operator: 0,
         operators: init_operators(config.operators()),
         choice_state: ChooseAction(0),
+    }
+}
+/// Gets (firewall mod, hacker_multiplier) depending on difficulty
+fn difficulty_mod(difficulty: &Difficulty) -> (usize, usize) {
+    match difficulty {
+        Easy => (3, 6),
+        Normal => (2, 7),
+        Hard => (1, 7),
+        Heroic => (0, 7),
     }
 }
 
@@ -61,21 +64,48 @@ mod tests {
     use crate::defs;
     use crate::defs::{OperatorType, NO_HACKER};
     use arrayvec::ArrayVec;
-    // TODO: use test_case::test_case;
+    use test_case::test_case;
 
-    #[test]
-    fn sets_up_game() {
-        let operator_order = [OperatorType::Biggs, OperatorType::Sniper];
-        // TODO: use test_case, Parameterize num of operators + difficulty
-        let config =
-            GameConfig::new(Difficulty::Normal, ArrayVec::from_iter(operator_order)).unwrap();
+    #[test_case(1, Difficulty::Easy)]
+    #[test_case(2, Difficulty::Easy)]
+    #[test_case(3, Difficulty::Hard)]
+    #[test_case(4, Difficulty::Heroic)]
+    #[test_case(7, Difficulty::Heroic)]
+    #[test_case(7, Difficulty::Easy)]
+    #[test_case(7, Difficulty::Normal)]
+    fn sets_up_game(operators: usize, difficulty: Difficulty) {
+        let operator_order = [
+            OperatorType::Stone,
+            OperatorType::Sniper,
+            OperatorType::Rogue,
+            OperatorType::Biggs,
+            OperatorType::Admin,
+            OperatorType::Charm,
+            OperatorType::Rich,
+        ];
+        let chosen_operators = &operator_order[0..operators];
+        let (firewall_mod, hacker_mult) = difficulty_mod(&difficulty);
+
+        let config = GameConfig::new(
+            difficulty,
+            ArrayVec::from_iter(chosen_operators.iter().map(|x| *x)),
+        )
+        .unwrap();
 
         let state = setup_game(&config);
-        assert_eq!(state.firewalls(), 4, "firewalls = operators + 2");
+        assert_eq!(
+            state.firewalls(),
+            (operators + firewall_mod) as u8,
+            "firewalls = operators + 2"
+        );
         assert_eq!(state.databases(), [true; 3]);
         assert_eq!(state.webservices(), [true; 6]);
 
-        assert_eq!(state.hackers().len(), 14, "hackers = operators * 7");
+        assert_eq!(
+            state.hackers().len(),
+            operators * hacker_mult,
+            "hackers = operators * 7"
+        );
         for hacker in state.hackers().iter() {
             assert!(!hacker.face_up, "face down");
             let hacker = defs::hacker(hacker.hacker);
@@ -87,7 +117,7 @@ mod tests {
         assert_eq!(state.round(), 0);
         assert_eq!(state.active_operator(), 0);
 
-        assert_eq!(state.operators().len(), 2);
+        assert_eq!(state.operators().len(), operators);
         for (i, operator) in state.operators().iter().enumerate() {
             assert_eq!(operator.secure_slots(), [NO_HACKER; 3]);
             assert!(operator.backtrace_list().is_empty());
